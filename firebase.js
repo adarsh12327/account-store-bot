7a7bb176f814217c5857d67b8937fe0df9fdcd15
Adarsh Patel <kumaradarshpatel30@gmail.com>
2026-09-13 17:35:00 +0530

    Use Firebase service account environment variable

--- firebase.js
+++ firebase.js
@@ -1,7 +1,21 @@
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./serviceAccountKey.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+  try {
+    serviceAccount = JSON.parse(
+      process.env.FIREBASE_SERVICE_ACCOUNT_JSON
+    );
+  } catch (err) {
+    throw new Error(
+      "Invalid FIREBASE_SERVICE_ACCOUNT_JSON environment variable"
+    );
+  }
+} else {
+  serviceAccount = require("./serviceAccountKey.json");
+}
 
 if (!getApps().length) {
   initializeApp({
